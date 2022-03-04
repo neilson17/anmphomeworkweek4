5e629ce161a01a28bd19eb8ec5024f0ab9b20d76
neilson17 <88655257+neilson17@users.noreply.github.com>
2022-03-04 22:19:44 +0700

    init

---
+++ app/src/main/java/com/ubaya/advweek4/model/Model.kt
@@ -0,0 +1,9 @@
+package com.ubaya.advweek4.model
+
+data class Student(
+    var id: String?,
+    var name: String?,
+    var dob: String?,
+    var phone: String?,
+    var photoURL: String?
+)
---
+++ app/src/main/java/com/ubaya/advweek4/view/StudentListFragment.kt
@@ -0,0 +1,56 @@
+package com.ubaya.advweek4.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ubaya.advweek4.R
+import com.ubaya.advweek4.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.fragment_student_list.*
+
+class StudentListFragment : Fragment() {
+    private lateinit var viewModel :ListViewModel
+    private val studentListAdapter = StudentListAdapter(arrayListOf())
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_student_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = studentListAdapter
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.studentsLiveData.observe(viewLifecycleOwner){
+            studentListAdapter.updateStudentList(it)
+        }
+
+        viewModel.studentsLoadErrorLiveData.observe(viewLifecycleOwner){
+            textError.visibility = if (it) View.VISIBLE else View.GONE
+        }
+
+        viewModel.loadingLiveData.observe(viewLifecycleOwner){
+            if (it) { //sedang loading
+                recyclerView.visibility = View.GONE
+                progressLoad.visibility = View.VISIBLE
+            }
+            else {
+                recyclerView.visibility = View.VISIBLE
+                progressLoad.visibility = View.GONE
+            }
+        }
+    }
+}
